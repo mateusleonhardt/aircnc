e04895c2ab58e5fb7094915b5e75dfc909b24971
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2019-12-09 16:22:25 -0300

    Frontend added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +12,11 @@ mongoose.connect('mongodb+srv://mateusl91:mateusl91@cluster0-ge0wa.mongodb.net/a
     useUnifiedTopology: true,
 });
 
+// O cors deve ser adicionado para evitar que outras aplicações consumam os dados da api
+// Para garantir que somente o meu site, por exemplo, consuma as informações, deve ser passado um objeto como "{ origin: 'http://localhost:3333'}"
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333); 
