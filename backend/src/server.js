e1c58c931c53fe53a8bb7ed4e3fa730ac4b8a77d
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2019-12-17 12:02:46 -0300

    Finished application

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,34 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://mateusl91:mateusl91@cluster0-ge0wa.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 // O cors deve ser adicionado para evitar que outras aplicações consumam os dados da api
@@ -19,4 +40,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333); 
+server.listen(3333); 
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, AsyncStorage, Image, Platform, StatusBar} from 'react-native';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, StyleSheet, ScrollView, AsyncStorage, Image, Platform, StatusBar} from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -7,6 +8,18 @@ import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socket.io('http://192.168.5.6:3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => {
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+            })
+        })
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storageTechs => {
